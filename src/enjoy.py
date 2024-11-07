717dddddd8f8edba4171710497af4724b72473e8
Bartłomiej Krówka <157894166+bkrowka@users.noreply.github.com>
2024-11-07 16:05:01 +0100

    refactor: #dev Infinite enjoy loop.

--- src/enjoy.py
+++ src/enjoy.py
@@ -2,11 +2,14 @@ from gym import Env
 from stable_baselines3.common.base_class import BaseAlgorithm
 
 
-def enjoy(model: BaseAlgorithm, env: Env, num_episodes: int = 5) -> None:
-    for episode in range(num_episodes):
-        obs = env.reset()
-        done = False
+def enjoy(model: BaseAlgorithm, env: Env) -> None:
+    try:
+        while True:
+            obs = env.reset()
+            done = False
 
-        while not done:
-            action, _states = model.predict(observation=obs, deterministic=True)
-            obs, reward, done, info = env.step(int(action))
+            while not done:
+                action, _states = model.predict(observation=obs, deterministic=True)
+                obs, reward, done, info = env.step(int(action))
+    except KeyboardInterrupt:
+        print("Enjoy stopped.")
