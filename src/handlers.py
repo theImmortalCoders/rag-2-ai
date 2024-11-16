07a7a33dc4c7b9cc9e12c24aa49b618f8dd060a4
Marcin Bator <marcinbator.ofc@gmail.com>
2024-11-16 21:11:58 +0100

    feat: #dev guest users workflow

--- src/handlers.py
+++ src/handlers.py
@@ -1,51 +1,12 @@
 import json
-import os
-from urllib.parse import parse_qs, urlparse
+from collections import deque
+from typing import Callable
 
 import numpy as np
-import requests
-from dotenv import load_dotenv
-from tornado.websocket import WebSocketHandler
 from stable_baselines3.common.base_class import BaseAlgorithm
+
+from src.api import BaseHandler
 from src.env_sim.general import state_stack, action_map
-from tornado.web import RequestHandler
-from typing import List, Tuple, Type, Callable
-from collections import deque
-
-
-def verify_jwt(token):
-    load_dotenv()
-    TOKEN_VERIFY_URL = os.getenv('TOKEN_VERIFY_URL')
-    headers = {"Authorization": "Bearer " + token}
-    
-    try:
-        response = requests.get(TOKEN_VERIFY_URL, headers=headers, timeout=2)
-        print(response)
-        return response.status_code == 200
-    except requests.RequestException as e:
-        print(f"JWT verification failed: {e}")
-        return False
-
-
-class BaseHandler(WebSocketHandler):
-
-    def check_origin(self, origin):
-        return True
-
-    def open(self):
-        query_params = parse_qs(urlparse(self.request.uri).query)
-        token = query_params.get("jwt", [None])[0]
-
-        if not token or not verify_jwt(token):
-            self.close(code=401, reason="Unauthorized")
-            return
-        print("WebSocket connection opened and authenticated")
-
-    def on_close(self):
-        print("WebSocket connection closed")
-
-    def on_message(self, message):
-        pass
 
 
 class AiHandler(BaseHandler):
@@ -68,8 +29,8 @@ class AiHandler(BaseHandler):
         self.move_last = move_last
 
     def on_close(self):
+        super().on_close()
         self.states.clear()
-        print("WebSocket connection closed")
 
     def on_message(self, message):
         data = json.loads(message)
@@ -86,44 +47,3 @@ class AiHandler(BaseHandler):
         )
         if move is not None:
             self.write_message(json.dumps({'move': move, 'start': 1}))
-
-
-class RoutesHandler(RequestHandler):
-    def set_default_headers(self):
-        load_dotenv()
-        allowed_origins = os.getenv("ALLOWED_ORIGINS").split(',')
-        
-        origin = self.request.headers.get("Origin")
-        if origin in allowed_origins:
-            self.set_header("Access-Control-Allow-Origin", origin)
-        else:
-            self.set_header("Access-Control-Allow-Origin", "null")
-        
-        self.set_header("Access-Control-Allow-Credentials", "true")
-        self.set_header("Access-Control-Allow-Headers", "x-requested-with, Authorization")
-        self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-
-    def options(self):
-        self.set_status(204)
-        self.finish()
-
-    def initialize(self, routes: List[Tuple[str, Type, dict]]):
-        self.routes = routes
-
-    def get(self):
-        routes_info = []
-        for route in self.routes:
-            path = route[0][:-1]
-            last_dash_index = path.rfind('-')
-            last_slash_index = path.rfind('/')
-            last_index = max(last_dash_index, last_slash_index)
-
-            if last_index != -1:
-                url_type = path[last_index + 1:].upper()
-            else:
-                url_type = path.upper()
-
-            routes_info.append({"path": route[0], "name": url_type})
-
-        self.write(json.dumps(routes_info))
-
